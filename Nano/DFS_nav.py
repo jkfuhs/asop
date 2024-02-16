02795e84aa6c6563f0a4e5e3d079eddbbbbcbec3
jkfuhs <fuhs.jk@gmail.com>
2024-02-16 10:52:30 -0800

    Fixed "int" in python

--- Nano/DFS_nav.py
+++ Nano/DFS_nav.py
@@ -1,5 +1,6 @@
 import serial
 import struct
+import ctypes
 
 # Replace with port name
 port = "/dev/ttyUSB0"
@@ -8,9 +9,11 @@ baud_rate = 115200
 # Create serial object
 ser = serial.Serial(port, baud_rate, timeout=1)
 
-# Define command strucutre
+# Define command structure
 command_format = 'BHH'
 reply_format = 'llH'
+
+
 
 # Define the command values
 command_data = {
@@ -18,7 +21,7 @@ command_data = {
     'REVERSE': 1,
     'LEFT': 2,
     'RIGHT': 3,
-    'GETGPTS': 4
+    'GETGPS': 4
 }
 
 # Function to create a command packet
@@ -29,22 +32,3 @@ def create_command_packet(command, value, unique_id):
 def is_reply_ready():
     return ser.in_waiting >= struct.calcsize(reply_format)
 
-i = 1
-while 1:
-    # Attempt to send a command
-    command_packet = create_command_packet('FORWARD', 10, i)    
-    i = i+1
-    ser.write(command_packet)
-    
-    # Wait unil reply is sent back
-    while not is_reply_ready():
-        pass
-
-    # Read and extract the reply packet
-    received_data = ser.read(struct.calcsize(reply_format))
-    latitude, longitude, unique_id = struct.unpack(reply_format, received_data)
-
-    # Print unpacked data
-    print(f"Latitude: {latitude}, Longitude: {longitude}, Unique ID: {unique_id}")
-
-ser.close()
